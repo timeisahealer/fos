9b944df6f227b1e2c6e17cd7181b16dc218c8218
jysim3 <30685486+jysim3@users.noreply.github.com>
2018-05-12 03:45:56 +1000

    Current location with map

--- Proto/App.js
+++ Proto/App.js
@@ -1,6 +1,7 @@
-import {Component, React } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Event from './Event';
+
+import React, { Component } from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 
 class GeolocationExample extends Component {
   constructor(props) {
@@ -10,9 +11,23 @@ class GeolocationExample extends Component {
       latitude: null,
       longitude: null,
       error: null,
+      region: {
+                latitude: -33.8701062,
+                longitude: 151.2076937,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              },
     };
   }
 
+
+    onRegionChange(region) {
+        console.log(region);
+        console.log(this.state.region);
+        this.setState({
+            region: region
+        });
+    }
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -32,28 +47,26 @@ class GeolocationExample extends Component {
       <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
+            <MapView style={styles.map}
+                    showsUserLocation={ true }
+                    region={ this.state.region }
+                    onRegionChange={ region => this.setState({region}) }
+                    onRegionChangeComplete={ region => this.setState({region}) }
+            />
         {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
     );
   }
 }
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <GeolocationExample></GeolocationExample>
-        <Event></Event>
-      </View>
-    );
-  }
-}
+export default GeolocationExample;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create ({
+    map: {
+        width: '100%',
+        height: 500,
+        bottom: 0,
+        left: 0,
+        right: 0
+     },
+})
