2607f2f80bf0a6010f669c18dbaa58dfaaf75ab0
Sebastian Chua <sebastian.a.chua@gmail.com>
2018-05-11 22:42:18 +1000

    Testing card

--- Proto/App.js
+++ Proto/App.js
@@ -7,7 +7,14 @@ export default class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            events: [
+                {
+                    title:"CSE Barbeque",
+                    description: "Weekly Barbeque with the nice CSE Peeps YEAYAH",
+                    // location: "John Lions Garden",
+                    // date: "19-04-2018"
+                }
+            ]
         };
     }
 
@@ -15,16 +22,22 @@ export default class App extends Component {
         return (
             <List>
                 <FlatList
-                    data={ this.state.data }
-                    renderItem={ ({ item }) => (
+                    data={ this.state.events }
+                    renderItem={ ({ event }) => return (
 
-                        
-
-                    )
-
-
+                            <Card title={event.title}>
+                                <Text style={{marginBottom: 10}}>
+                                    {event.description}
+                                </Text>
+                                <Button
+                                icon={{name: 'code'}}
+                                backgroundColor='#03A9F4'
+                                fontFamily='Lato'
+                                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                                title='VIEW NOW' />
+                            </Card>
+                        )
                     }
-
                 />
             </List>
         );
