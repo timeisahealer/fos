2ad2c35861313f9c271c929253470ae6e58f5878
Sebastian Chua <sebastian.a.chua@gmail.com>
2018-05-11 22:18:13 +1000

    Modified list view

--- Proto/App.js
+++ Proto/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { List, ListItems, Card, Button } from 'react-native-elements'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+
+    render() {
+        return (
+            <List>
+                <FlatList
+                    data={ this.state.data }
+                    renderItem={ ({ item }) => (
+
+                        
+
+                    )
+
+
+                    }
+
+                />
+            </List>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+},
 });
